5cbe1d11b993c1f34186451fc4fb620d7d43c422
Luis <luisandrino3@gmail.com>
2023-05-05 18:53:39 -0600

    Añadidas animaciones

--- js/main.js
+++ js/main.js
@@ -1,71 +1,118 @@
 const productos = [
     {
-        texto: "Tomacorriente Q200",
-        img: "img/dasd.png"
+        texto: "Tomacorriente",
+        img: "img/dasd.png",
+        precio: "Q200"
     }, {
-        texto: "Bombilla - Q300",
-        img: "img/Bombilla-Incandescente-100W-Opaca-Sylvania_97df9c5.png"
+        texto: "Bombilla",
+        img: "img/Bombilla-Incandescente-100W-Opaca-Sylvania_97df9c5.png",
+        precio: "Q300"
     }, 
     {
-        texto: "Tester - Q500",
-        img: "img/MUT-830-a.png"
+        texto: "Tester",
+        img: "img/MUT-830-a.png",
+        precio: "Q500"
     }, 
     {
-        texto: "Plug - Q602",
-        img: "img/71ItIEIrIML.png"
+        texto: "Plug",
+        img: "img/71ItIEIrIML.png",
+        precio: "Q602"
     },
     {
-        texto: "Flaponera Q200",
-        img: "img/788174_1.png"
+        texto: "Flaponera",
+        img: "img/788174_1.png",
+        precio: "Q200"
     }, {
-        texto: "CortaFuego - Q300",
-        img: "img/FV-AA.png"
+        texto: "CortaFuego",
+        img: "img/FV-AA.png",
+        precio: "Q300"
     }, 
 ]
 
-const cambiar = document.querySelector(".next-item")
-const retroceder = document.querySelector(".previous-item")
-const img = document.querySelector(".primer-item");
-const text = document.querySelector(".primer-text") 
-let contador = 1;
-img.setAttribute("src", productos[0].img);
-text.textContent = productos[0].texto;
+function elementos(productos){
+    //Creando Elementos HTML
+    const divMainItems = document.createElement("div");
+    divMainItems.classList.add("main-items");
+    const imgElement = document.createElement("img");
+    const parrafoElement = document.createElement("p");
+    const btnElements = document.createElement("button");
+    const divEditar = document.querySelector("#editar")
+
+    //Creando los botones
+    const cambiar = document.querySelector(".next-item")
+    const retroceder = document.querySelector(".previous-item")
+
+    //Asignando valores al html
+    imgElement.src = productos[0].img;
+    parrafoElement.textContent = `${productos[0].texto} - ${productos[0].precio}`;
+    btnElements.textContent = `Info ${productos[0].texto}`
+
+    //Eventos
+    let contador = 1;
+    cambiar.addEventListener("click", cambiarPantalla);
+    function cambiarPantalla(){
+        
+        divMainItems.classList.remove("transition-l");
+        divMainItems.classList.add("transition");
+
+        divMainItems.addEventListener("animationend", function() {
+        divMainItems.classList.remove("transition");
+    });
 
 
-cambiar.addEventListener("click", cambiarPantalla);
+        if(contador >= 0 && contador < productos.length){
+                imgElement.src = productos[contador].img;
+                parrafoElement.textContent = `${productos[contador].texto} - ${productos[contador].precio}`;
+                btnElements.textContent = `Info ${productos[contador].texto}`
+                console.log(contador)
+                contador++;
 
-function cambiarPantalla(){
-    
-    if(contador >= 0 && contador != productos.length){
-        img.setAttribute("src", productos[contador].img);
-        text.textContent = productos[contador].texto;
-        contador++;
-    } else if (contador == productos.length){
-        contador = 0;
-        img.setAttribute("src", productos[contador].img);
-        text.textContent = productos[contador].texto;
-        contador++;
+                if(contador >= productos.length){
+                    contador = 0;
+                }
+
+        } 
+        
     }
 
-    console.log(contador);
+
+    retroceder.addEventListener("click", retrocederPantalla);
+
+    function retrocederPantalla() {
+
+        divMainItems.classList.remove("transition");
+        divMainItems.classList.add("transition-l");
+
+        divMainItems.addEventListener("animationend", function() {
+        divMainItems.classList.remove("transition-l");
+        });
+
+        if (contador > 0 && contador <= productos.length) {
+            contador--;
+            imgElement.src = productos[contador].img;
+            parrafoElement.textContent = `${productos[contador].texto} - ${productos[contador].precio}`;
+            btnElements.textContent = `Info ${productos[contador].texto}`;
+            console.log(contador);
+        } else if (contador <= 0) {
+            contador = productos.length - 1;
+            imgElement.src = productos[contador].img;
+            parrafoElement.textContent = `${productos[contador].texto} - ${productos[contador].precio}`;
+            btnElements.textContent = `Info ${productos[contador].texto}`;
+            console.log(contador);
+        }
 }
 
-retroceder.addEventListener("click", retrocederPantalla);
 
-function retrocederPantalla(){
-    
-    if(contador>= 0 && contador <= productos.length){
-        --contador
-        img.setAttribute("src", productos[contador].img);
-        text.textContent = productos[contador].texto;
-    } else {
-        contador = productos.length;
-        img.setAttribute("src", productos[--contador].img);
-        text.textContent = productos[contador].texto;    
-    }
-  
-   
+    //Uniendo el html
+    divMainItems.append(imgElement, parrafoElement);
+    divEditar.append(divMainItems, btnElements);
 }
 
-//TODO
+elementos(productos);
+
+
+
+
+
+//TODO:
 //Terminar el contador
